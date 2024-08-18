cb14de7ef2ebd41b4b6c32f943743eb7692aac8a
falljohanna <167862819+falljohanna@users.noreply.github.com>
2024-08-18 16:07:29 +0200

    Added AI API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 let poemFormElement = document.querySelector("#poem-generator-form")
 poemFormElement.addEventListener("submit", generatePoem);
 
+
 function generatePoem(event) {
     event.preventDefault();
-   
+    
+    let instructionsInput = document.querySelector ("#user-instructions")
+    let context = "Generate a 4 line poem and separate each line with <br>"
+    let prompt = `Generate a short poem about ${instructionsInput}`
+    let apikey = "4d32ebdo522bet9b3a02cc31eef7c6bf"
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`
+    
+    console.log ("Generating poem")
+    console.log (`Prompt: ${prompt}`)
+    console.log (`Context: ${context}`)
+    console.log (`user instructions: ${instructionsInput}`)
+    
+    axios.get(apiUrl).then(displayPoem)
+    
+}
+
+function displayPoem (response) {
+    
     new Typewriter("#poem", {
-        strings: "Generating roses and violets...",
+        strings: response.data.answer,
         autoStart: true,
-        delay: 50,
+        delay: 20,
         cursor: "",
       });
 }
